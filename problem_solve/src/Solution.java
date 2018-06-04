858bb8a282d3e90aff238b56bbf4350e27da69c5
nadara9209 <nadara9209@gmail.com>
2018-06-04 20:59:05 +0900

    solve swea4193

--- problem_solve/src/Solution.java
+++ problem_solve/src/Solution.java
@@ -1,106 +1,108 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Solution {
-	static char[] OPERS = {'+', '-', '*', '/'};
-	// 피연산자 배열
-	static int[] NUMARR;
-	// 연산자의 수
-	static int NOPER = 0;
+	static int[][] Map;
+	static boolean[][] Visited;
+	static int[] Dx = {-1, 0, 1, 0};
+	static int[] Dy = {0, -1, 0, 1};
+	static Point StartP;
+	static Point EndP;
+	static int N;
+	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int T = scan.nextInt();
 		for(int tc = 1; tc <= T; ++tc) {
-			int operArrLen = OPERS.length;
-			int[] nOperArr = new int[operArrLen];
-			int N = scan.nextInt();
-			NUMARR = new int[N];
-			NOPER = N - 1;
-			// operArr 구성
-			for(int i = 0; i < operArrLen; ++i) {
-				int nOper = scan.nextInt();
-				nOperArr[i] = nOper;
+			N = scan.nextInt();
+			Map = new int[N][N];
+			Visited = new boolean[N][N];
+			for(int row = 0; row < N; ++row) {
+				for(int col = 0; col < N; ++col) {
+					Map[row][col] = scan.nextInt();
+					Visited[row][col] = false;
+				}
 			}
-			// numArr 구성
-			for(int i = 0; i < N; ++i) {
-				int tmpNum = scan.nextInt();
-				NUMARR[i] = tmpNum;
-			}
-			System.out.println("#" + tc + " " + solve(nOperArr));
+			
+			int startX = scan.nextInt();
+			int startY = scan.nextInt();
+			int endX = scan.nextInt();
+			int endY = scan.nextInt();
+			
+			StartP = new Point(startX, startY, 0);
+			EndP = new Point(endX, endY, 0);
+			
+			System.out.println("#" + tc + " " + solve());
 		}
 		scan.close();
 	}
 
-	private static int solve(int[] nOperArr) {
-		// 연산자 갯수를 통해 연산자를 담은 operArr을 만든다.
-		ArrayList<Character> candidateList = new ArrayList<>();
-		for(int i = 0; i < nOperArr.length; ++i) {
-			int nOper = nOperArr[i];
-			for(int j = 0; j < nOper; ++j) {
-				candidateList.add(OPERS[i]);
+	private static int solve() {
+		Point retP = null;
+		// 큐에 넣고
+		PriorityQueue<Point> q = new PriorityQueue<>();
+		q.offer(StartP);
+		Visited[StartP.x][StartP.y] = true;
+		
+		// 큐가 다 빌 때까지
+		while(!q.isEmpty()) {
+			// 확인할 point를 큐에서 빼고
+			Point p = q.poll();
+			Visited[p.x][p.y] = true;
+			if(p.x == EndP.x && p.y == EndP.y) {
+				retP = p;
+				break;
+			}
+			// 현재 시각
+			int currTime = p.t;
+			// point의 모든 근접배열로 이동
+			for(int i = 0; i < 4; ++i) {
+				int nextX = p.x + Dx[i];
+				int nextY = p.y + Dy[i];
+				int nextTime = currTime + 1;
+				
+				// 벽
+				if(nextX < 0 || nextY < 0 || nextX >= N || nextY >= N) {
+					continue;
+				}
+				// 장애물
+				if(Visited[nextX][nextY] || Map[nextX][nextY] == 1) {
+					continue;
+				}
+				
+				int tOffset = 0;
+				if(Map[nextX][nextY] == 2) {
+					tOffset = 2 - (currTime % 3);
+				}
+				
+				q.offer(new Point(nextX, nextY, nextTime + tOffset));
 			}
 		}
 		
-		// 선택된 리스트
-		ArrayList<Character> selectedList = new ArrayList<>();
-		// 결과를 저장할 리스트
-		ArrayList<Integer> retList = new ArrayList<>();
-		// 입력 받은 연산자들로 구성할 수 있는 모든 경우를 계산
-		operateAllOperStrCases(selectedList, candidateList, retList);
-		
-		Collections.sort(retList);
-		int minVal = retList.get(0);
-		int maxVal = retList.get(retList.size()-1);
-		return maxVal - minVal;
+		return Visited[EndP.x][EndP.y] ? retP.t : -1;
 	}
+}
+
+class Point implements Comparable<Point> {
+	public int x;
+	public int y;
+	public int t;
 	
-	private static void operateAllOperStrCases(ArrayList<Character> selectedList, ArrayList<Character> candidateList, ArrayList<Integer> retList) {
-		// 선택된 리스트가 연산자의 갯수와 같다면 연산하여 저장
-		if(selectedList.size() == NOPER) {
-			retList.add(operate(selectedList));
-		}
-		
-		int candidateSize = candidateList.size();
-		for(int i = 0; i < candidateSize; ++i) {
-			char tmpChar = candidateList.get(i);
-			// 중복 가지치기
-			if(i + 1 < candidateSize && tmpChar == candidateList.get(i+1)) {
-				continue;
-			}
-			// 추가
-			selectedList.add(tmpChar);
-			// 다시 인자로 날려줄 subCandidateList 생성
-			ArrayList<Character> subCandidateList = new ArrayList<>(candidateList.subList(0, i));
-			subCandidateList.addAll(new ArrayList<>(candidateList.subList(i+1, candidateList.size())));
-			// 추가된 selectedList와 subCandidateList 재귀
-			operateAllOperStrCases(selectedList, subCandidateList, retList);
-			// 해당 수행 후 연산자 수거.
-			selectedList.remove(selectedList.size()-1);
-		}
+	Point(int x, int y, int t) {
+		this.x = x;
+		this.y = y;
+		this.t = t;
 	}
 
-	private static int operate(ArrayList<Character> selectedList) {
-		int ret = NUMARR[0];
-		for(int i = 0; i < selectedList.size(); ++i) {
-			char oper = selectedList.get(i);
-			switch (oper) {
-			case '+':
-				ret = ret + NUMARR[i+1];
-				break;
-			case '-':
-				ret = ret - NUMARR[i+1];
-				break;
-			case '*':
-				ret = ret * NUMARR[i+1];
-				break;
-			case '/':
-				ret = ret / NUMARR[i+1];
-				break;
-			default:
-				break;
-			}
+	public int compareTo(Point o) {
+		if(t - o.t > 0) {
+			return 1;
 		}
-		return ret;
+		else if (t - o.t < 0) {
+			return -1;
+		}
+		else {
+			return 0;
+		}
 	}
 }

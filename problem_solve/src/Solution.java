8d6005016bc8080dbf75eaefe1a10b9cd1bf94b1
nadara9209 <nadara9209@gmail.com>
2018-06-21 08:11:53 +0900

    modify variables, comments

--- problem_solve/src/Solution.java
+++ problem_solve/src/Solution.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
  
 class Solution {
-	static int[][] Memo = new int[1001][1001];
+	static int[][] MEMO = new int[1001][1001];
 	static String StrA;
 	static String StrB;
     public static void main(String[] args) {
@@ -16,32 +16,35 @@ class Solution {
     }
 	
     private static int solve() {
-    	// 패딩 채우기
+    	int answer = countLCS();
+    	return answer;
+	}
+
+	private static int countLCS() {
+		// 패딩 채우기
     	int lenOfStrA = StrA.length();
     	int lenOfStrB = StrB.length();
     	for(int col = 0; col <= lenOfStrA; ++col) {
-    		Memo[lenOfStrB][col] = 0;
+    		MEMO[lenOfStrB][col] = 0;
     	}
     	for(int row = 0; row <= lenOfStrB; ++row) {
-    		Memo[row][lenOfStrA] = 0;
+    		MEMO[row][lenOfStrA] = 0;
     	}
     	
-    	// 일반 항목 채우기
+    	// 점화식 결과 채우기
     	int endIdOfStrA = lenOfStrA - 1;
     	int endIdOfStrB = lenOfStrB - 1;
     	for(int row = endIdOfStrB; row >= 0; --row) {
     		for(int col = endIdOfStrA; col >= 0; --col) {
     			int match = (StrA.charAt(col) == StrB.charAt(row)) ? 1 : 0;
-    			int right = Memo[row][col+1];
-    			int bottom = Memo[row+1][col];
-    			int diagonal = Memo[row+1][col+1] + match;
+    			int right = MEMO[row][col+1];
+    			int down = MEMO[row+1][col];
+    			int diagonal = MEMO[row+1][col+1] + match;
     			
-    			Memo[row][col] = Math.max(Math.max(diagonal, right), bottom);
+    			MEMO[row][col] = Math.max(Math.max(right, down), diagonal);
     		}
     	}
     	
-    	// 결과 구하기
-    	int ret = Memo[0][0];
-    	return ret;
+    	return MEMO[0][0];
 	}
 }

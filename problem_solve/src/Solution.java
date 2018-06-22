30416bd2dabddb7df21af0f6bd908a05fcbadd3b
nadara9209 <nadara9209@gmail.com>
2018-06-22 16:34:59 +0900

    swap code with swea 4014

--- problem_solve/src/Solution.java
+++ problem_solve/src/Solution.java
@@ -1,49 +1,52 @@
 import java.util.Scanner;
 
 public class Solution {
-	static int[][] map;
 	static int N;
-	static int X;
+	static int B;
+	static int[] Heights;
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int T = scan.nextInt();
-		for(int tc = 1; tc < T; ++tc) {
+		for(int tc = 1; tc <= T; ++tc) {
 			N = scan.nextInt();
-			X = scan.nextInt();
+			B = scan.nextInt();
 			
-			map = new int[N][N];
-			for(int row = 0; row < N; ++row) {
-				for(int col = 0; col < N; ++col) {
-					int val = scan.nextInt();
-					map[row][col] = val;
-				}
+			Heights = new int[N];
+			for(int i = 0; i < N; ++i) {
+				int height = scan.nextInt();
+				Heights[i] = height;
 			}
-			
-			System.out.println("#" + tc + " " + solve());
+			int answer = solve();
+			System.out.println("#" + tc + " " + answer);
 		}
 		scan.close();
 	}
+	
 	private static int solve() {
-		int answer = 0;
-		// 행간의 한 라인을 받아서 따로 저장하고
-		// 행 
-		for(int row = 0; row < N; ++row) {
-			for(int col = 0; col < N; ++col) {
-				
+		int minGap = 0;
+		
+		int sum = 0;
+		int index = 0;
+		minGap = makeAllCases(sum, index);
+		
+		return minGap;
+	}
+	
+	private static int makeAllCases(int sum, int index) {
+		if(index >= N) {
+			if(sum >= B) {
+				return sum - B;
+			}
+			else {
+				return Integer.MAX_VALUE;
 			}
 		}
 		
-		// 열
-		for(int col = 0; col < N; ++col) {
-			for(int row = 0; row < N; ++row) {
-				
-			}
-		}
-		
-		// 저장된 한 라인을 검사하여 
-		// 활주로 건설 여부 확인하고 
-		// 여부에 맞게 cnt를 증가 시킨다.
-		
-		return answer;
+		// 선택
+		int sumOfCaseA = makeAllCases(sum + Heights[index], index+1);
+		// 미선택
+		int sumOfCaseB = makeAllCases(sum, index+1);
+	
+		return Math.min(sumOfCaseA, sumOfCaseB);
 	}
 }

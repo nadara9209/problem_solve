4926af13c4de8282ed1057934282c88b67367744
nadara9209 <nadara9209@gmail.com>
2018-06-26 05:12:37 +0900

    solve swea2806

--- problem_solve/src/Solution.java
+++ problem_solve/src/Solution.java
@@ -1,72 +1,87 @@
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Solution {
-	static int N;
-	static int X;
-	static int[][] map;
+	static int[] Drow = {-1,  1,  0,  0, -1, -1,  1,  1};
+	static int[] Dcol = { 0,  0, -1,  1, -1,  1, -1,  1};
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int T = scan.nextInt();
-		for(int tc = 1; tc < T; ++tc) {
-			N = scan.nextInt();
-			X = scan.nextInt();
-			// create
-			map = new int[N][N];
-			for(int row = 0; row < N; ++row) {
-				for(int col = 0; col < N; ++col) {
-					int height = scan.nextInt();
-					map[row][col] = height;
-				}
-			}
-			
-			// 가능한 경우 수들의 합
-			int answer = sumOfPossibleCases();
-			System.out.println("#" + tc + " " + answer);
+		for(int tc = 1; tc <= T; ++tc) {
+			int N = scan.nextInt();
+			int[][] map = new int[N][N];
+			System.out.println("#" + tc + " " + solve(map, N));
 		}
 		scan.close();
 	}
-
-	private static int sumOfPossibleCases() {
-		LinkedList<Point> pointList = new LinkedList<>();
-		
-		int cntOfPossibleCases = 0;
-		// 행
-		for(int row = 0; row < N; ++row) {
-			for(int col = 0; col < N; ++col) {
-				checkArr[col] = map[row][col];
-			}
-			if(isAvailable(checkArr)) {
-				cntOfPossibleCases++;
-			}
-		}
-		// 열
-		for(int col = 0; col < N; ++col) {
-			for(int row = 0; row < N; ++row) {
-				checkArr[row] = map[row][col];
-			}
-			if(isAvailable(checkArr)) {
-				cntOfPossibleCases++;
-			}
-		}
-		
-		return cntOfPossibleCases;
+	
+	private static int solve(int[][] map, int N) {
+		Board chessBoard = new Board(map, N);
+		int answer = countAllCases(chessBoard);
+		return answer;
 	}
-
-	// map의 한 행, 열을 받아와서 활주로 건설의 가능성 유무를 확인
-	private static boolean isAvailable(int[] checkArr) {
+	
+	private static int countAllCases(Board chessBoard) {
+		if(chessBoard.numOfQueens == 0) {
+			return 1;
+		}
 		
-		return false;
+		int nCases = 0;
+		int row = chessBoard.height - chessBoard.numOfQueens;
+		for(int col = 0; col < chessBoard.width; ++col) {
+			if(chessBoard.isQueenOk(row, col)) {
+				chessBoard.putDownQueen(row, col);
+				nCases += countAllCases(chessBoard);
+				chessBoard.putUpQueen(row, col);
+			}
+		}
+		
+		return nCases;
+	}
+	
+	static class Board {
+		int[][] map;
+		int numOfQueens;
+		int width;
+		int height;
+		
+		Board(int[][] map, int numOfQueen) {
+			this.map = map;
+			this.numOfQueens = numOfQueen;
+			this.width = map[0].length;
+			this.height = map.length;
+		}
+
+		private boolean isValid(int row, int col) {
+			return (row >= 0 && col >= 0 && row < this.height && col < this.width);
+		}
+		
+		private void score(int row, int col, int val) {
+			this.map[row][col] += val;
+			
+			for(int i = 0; i < 8; ++i) {
+				int currRow = row;
+				int currCol = col;
+				while(this.isValid(currRow+Drow[i], currCol+Dcol[i])) {
+					currRow += Drow[i];
+					currCol += Dcol[i];
+					this.map[currRow][currCol] += val;
+				}
+			}
+		}
+		
+		public void putUpQueen(int row, int col) {
+			this.score(row, col, -1);
+			this.numOfQueens++;
+		}
+
+		public void putDownQueen(int row, int col) {
+			this.score(row, col, 1);
+			this.numOfQueens--;
+		}
+
+		public boolean isQueenOk(int row, int col) {
+			return (this.map[row][col] == 0);
+		}
 	}
 }
-
-class Point {
-	int id;
-	boolean isUp;
-	
-	Point(int id, boolean isUp) {
-		this.id = id;
-		this.isUp = isUp;
-	}
-} 

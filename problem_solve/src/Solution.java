b05e50b3290618cee03af8ece8c6017eb4630f64
nadara9209 <nadara9209@gmail.com>
2018-07-05 09:19:55 +0900

    solve swea1215

--- problem_solve/src/Solution.java
+++ problem_solve/src/Solution.java
@@ -1,149 +1,67 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Solution {
-	static int[][] map;
-	static int N;
-	static int X;
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int T = scan.nextInt();
+		int T = 10;
 		for(int tc = 1; tc <= T; ++tc) {
-			N = scan.nextInt();
-			X = scan.nextInt();
-			
-			map = new int[N][N];
-			for(int row = 0; row < N; ++row) {
-				for(int col = 0; col < N; ++col) {
-					int val = scan.nextInt();
-					map[row][col] = (val * 10);
+			int N = scan.nextInt();
+			char[][] map = new char[8][8];
+			for(int row = 0; row < 8; row++) {
+				String str = scan.next();
+				char[] tempArr = str.toCharArray();
+				for(int col = 0; col < 8; col++) {
+					map[row][col] = tempArr[col];
 				}
 			}
 			
-			int answer = solve();
+			int answer = solve(map, N);
 			System.out.println("#" + tc + " " + answer);
 		}
 		scan.close();
 	}
-	
-	private static int solve() {
-		int numOfPossibleCaseWithRow = 0;
-		int numOfPossibleCaseWithCol = 0;
-		
-		for(int row = 0; row < N; ++row) {
-			int[] heightArr = new int[N];
-			for(int col = 0; col < N; ++col) {
-				heightArr[col] = map[row][col];
-			}
-			if(isPossible(heightArr)) {
-				numOfPossibleCaseWithRow++;
-			}else {
-			}
-		}
-		
-		for(int col = 0; col < N; ++col) {
-			int[] heightArr = new int[N];
-			for(int row = 0; row < N; ++row) {
-				heightArr[row] = map[row][col];
-			}
-			if(isPossible(heightArr)) {
-				numOfPossibleCaseWithCol++;
-			}
-			else {
+
+	private static int solve(char[][] map, int n) {
+		LinkedList<Character> checkList = new LinkedList<>();
+		int retCnt = 0;
+		// 가로
+		for(int row = 0; row < 8; ++row) {
+			checkList.clear();
+			for(int col = 0; col < 8; ++col) {
+				char val = map[row][col];
+				if(checkList.size() == n) {
+					checkList.removeFirst();
+				}
+				checkList.addLast(val);
+				if(checkList.size() == n && isPalindrome(checkList)) {
+					retCnt++;
+				}
 			}
 		}
-		
-		return numOfPossibleCaseWithRow + numOfPossibleCaseWithCol;
+		// 세로
+		for(int row = 0; row < 8; ++row) {
+			checkList.clear();
+			for(int col = 0; col < 8; ++col) {
+				char val = map[col][row];
+				if(checkList.size() == n) {
+					checkList.removeFirst();
+				}
+				checkList.addLast(val);
+				if(checkList.size() == n && isPalindrome(checkList)) {
+					retCnt++;
+				}
+			}
+		}
+		return retCnt;
 	}
 
-	private static boolean isPossible(int[] heightArr) {
-		List<Point> turnPointList = new ArrayList<>();
-		for(int i = 0; i < heightArr.length-1; ++i) {
-			int flag = heightArr[i] - heightArr[i+1];
-			// 둘의 높이차이가 1이상일때
-			if(Math.abs(flag) > 10) {
+	private static boolean isPalindrome(LinkedList<Character> checkList) {
+		for(int i = 0; i < (checkList.size() / 2); ++i) {
+			if(checkList.get(checkList.size()-1-i) != checkList.get(i)) {
 				return false;
 			}
-			if(flag == 10) {
-				// down
-				turnPointList.add(new Point(i+1, false));
-			}
-			else if (flag == -10) {
-				// up
-				turnPointList.add(new Point(i, true));
-			}
 		}
-		
-		for(int i = 0; i < turnPointList.size(); ++i) {
-			Point currP = turnPointList.get(i);
-			if(currP.isUp) {
-				int offset = currP.id;
-				offset -= (X-1);
-				if(isValid(offset)) {
-					build(currP, heightArr);
-				}
-				else {
-					return false;
-				}
-			}
-			else {
-				int offset = currP.id;
-				offset += (X-1);
-				if(isValid(offset)) {
-					build(currP, heightArr);
-				}
-				else {
-					return false;
-				}
-			}
-		}
-		
-		boolean flag = true;
-		for(int i = 0; i < heightArr.length-1; ++i) {
-			if(!(heightArr[i] % 10 == 1 && heightArr[i+1] % 10 == 1)) {
-				if(Math.abs(heightArr[i] - heightArr[i+1]) == 10) {
-					flag = false;
-				}
-			}
-			if(heightArr[i] % 10 > 1) {
-				flag = false;
-			}
-		}
-		
-		return flag;
-	}
-
-	private static void build(Point currP, int[] heightArr) {
-		if(currP.isUp) {
-			int cnt = 0;
-			int id = currP.id;
-			while(cnt < X) {
-				heightArr[id--] += 1;
-				cnt++;
-			}
-		}
-		else {
-			int cnt = 0;
-			int id = currP.id;
-			while(cnt < X) {
-				heightArr[id++] += 1;
-				cnt++;
-			}
-		}
-	}
-
-	private static boolean isValid(int offset) {
-		return (offset >= 0 && offset < N);
-	}
-}
-
-class Point {
-	int id;
-	boolean isUp;
-	
-	Point(int id, boolean isUp) {
-		this.id = id;
-		this.isUp = isUp;
+		return true;
 	}
 }

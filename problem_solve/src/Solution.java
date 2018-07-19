95556a808f8ad0bd7487d34167da62f1218cc51d
nadara9209 <nadara9209@gmail.com>
2018-07-19 17:21:58 +0900

    solving

--- problem_solve/src/Solution.java
+++ problem_solve/src/Solution.java
@@ -1,69 +1,33 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Arrays;
 import java.util.Scanner;
 
-class Solution {
+public class Solution {
+	static int[] numArr = new int[100000];
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int T = scan.nextInt();
 		for(int tc = 1; tc <= T; ++tc) {
 			int N = scan.nextInt();
-			int M = scan.nextInt();
-			int K = scan.nextInt();
-			
-			ArrayList<Integer>[] adjList = (ArrayList<Integer>[]) new ArrayList[N+1];
-			// create
-			for(int i = 0; i <= N; ++i) {
-				adjList[i] = new ArrayList<>();
+			for(int i = 0; i < N; ++i) {
+				int numVal = scan.nextInt();
+				numArr[i] = numVal;
 			}
 			
-			int tailVertex = 0;
-			int headVertex = 0;
-			for(int i = 0; i < M; ++i) {
-				tailVertex = scan.nextInt();
-				headVertex = scan.nextInt();
-				adjList[tailVertex].add(headVertex);
-			}
-			
-			int answer = solve(N, K, adjList);
+			long answer = solve(N);
 			System.out.println("#" + tc + " " + answer);
 		}
 		scan.close();
 	}
-
-	private static int solve(int n, int k, ArrayList<Integer>[] adjMatrix) {
-		Queue<Node> q = new LinkedList<>();
-		q.offer(new Node(1));
-		
-		while(!q.isEmpty()) {
-			Node node = q.poll();
-			int currTime = node.time;
-			if(node.tailVertex == n) {
-				if(currTime <= k) {
-					return currTime;
-				}
-			}
-			for(int id = 0; id < adjMatrix[node.tailVertex].size(); ++id) {
-				q.offer(new Node(adjMatrix[node.tailVertex].get(id), currTime+1));
-			}
-		}
-		
-		return -1;
-	}
-}
-
-class Node {
-	int tailVertex;
-	int time;
 	
-	Node(int tailVertex) {
-		this.tailVertex = tailVertex;
-		this.time = 0;
+	private static long solve(int n) {
+		int result = sumAllNumbers(n, result);
+		return result;
 	}
-	
-	Node(int tailVertex, int time) {
-		this.tailVertex = tailVertex;
-		this.time = time;
+
+	private static int sumAllNumbers(int n, int result) {
+		if()
+		Arrays.sort(numArr, 0, n);
+		
+		return 0;
 	}
 }

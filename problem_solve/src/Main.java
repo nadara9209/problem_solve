37631d3114193da8116832a9e1304806c4c6ebf4
nadara9209 <nadara9209@gmail.com>
2018-08-08 02:18:26 +0900

    modify combination feature

--- problem_solve/src/Main.java
+++ problem_solve/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -34,33 +35,35 @@ public class Main {
 	
 	private static int solve(LinkedList<Point> candidateList) {
 		// M개를 구성할 수 있는 모든 치킨집의 예를 구하고
-		LinkedList<Point> selectedList = new LinkedList<>();
-		int minSumOfDistance = makeAllCases(selectedList, candidateList);
+		ArrayList<Point> selectedList = new ArrayList<>();
+		int startId = 0;
+		int minSumOfDistance = makeAllCases(selectedList, candidateList, startId);
 		// 최소한의 치킨 거리를 도출한다.
 		return minSumOfDistance;
 	}
 
-	private static int makeAllCases(LinkedList<Point> selectedList, LinkedList<Point> candidateList) {
+	private static int makeAllCases(ArrayList<Point> selectedList, LinkedList<Point> candidateList, int currId) {
 		if(selectedList.size() == M) {
 			// 설정된 치킨 집으로 치킨거리의 합을 구한다.
 			return operateSumOfDistance(selectedList);
 		}
-		int ret = Integer.MAX_VALUE;
-		// 후보 리스트에서 뽑을 수 있는 갯수
-		int remainingSize = candidateList.size() - M + selectedList.size() + 1;
-		for(int i = 0; i < remainingSize; ++i) {
-			selectedList.add(candidateList.get(i));
-			LinkedList<Point> remainCandidateList = new LinkedList<>(candidateList.subList(i+1, candidateList.size()));
-			int tmp = makeAllCases(selectedList, remainCandidateList);
-			if(ret > tmp) {
-				ret = tmp;
-			}
-			selectedList.remove(selectedList.size()-1);
+		
+		if(currId >= candidateList.size()) {
+			return Integer.MAX_VALUE;
 		}
-		return ret;
+		
+		// 선택
+		selectedList.add(candidateList.get(currId));
+		int case1 = makeAllCases(selectedList, candidateList, currId+1);
+		selectedList.remove(selectedList.size()-1);
+		
+		// 미선택
+		int case2 = makeAllCases(selectedList, candidateList, currId+1);
+		
+		return Math.min(case1, case2);
 	}
 
-	private static int operateSumOfDistance(LinkedList<Point> selectedList) {
+	private static int operateSumOfDistance(ArrayList<Point> selectedList) {
 		Queue<Point> q = new LinkedList<>();
 		for(int i = 0; i < selectedList.size(); ++i) {
 			q.offer(selectedList.get(i));

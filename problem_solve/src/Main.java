c9b334297925ed789b38e0c2c34e74a153edbaa6
nadara9209 <nadara9209@gmail.com>
2018-08-30 09:20:09 +0900

    add bufferedReader

--- problem_solve/src/Main.java
+++ problem_solve/src/Main.java
@@ -1,7 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+//		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+//		String str = in.readLine();
+		
 		Scanner scan = new Scanner(System.in);
 		int N = scan.nextInt();
 		

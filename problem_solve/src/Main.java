1cf8c919b6a2dec55ce5bbbff414ee9d826c0de6
nadara9209 <nadara9209@gmail.com>
2018-09-01 09:44:29 +0900

    failed code

--- problem_solve/src/Main.java
+++ problem_solve/src/Main.java
@@ -8,50 +8,92 @@ public class Main {
 		int K = scan.nextInt();
 		
 		int[] numbers = new int[N];
-		for(int i = 0; i < N; ++i) {
+		for (int i = 0; i < N; ++i) {
 			numbers[i] = scan.nextInt();
 		}
 		
-		int nOfQ = M+K;
-		int[][] questions = new int[nOfQ][3];
-		for(int i = 0; i < nOfQ; ++i) {
+		int limit = M+K;
+		Command[] commands = new Command[limit];
+		for (int i = 0; i < limit; ++i) {
 			int command = scan.nextInt();
 			int src = scan.nextInt();
 			int dst = scan.nextInt();
-			
-			questions[i][0] = command;
-			questions[i][1] = src;
-			questions[i][2] = dst;
+			commands[i] = new Command(command, src, dst);
 		}
 		
-		solve(nOfQ, numbers, questions);
+		solve(numbers, commands, limit);
 		scan.close();
 	}
 
-	private static void solve(int nOfQ, int[] numbers, int[][] questions) {
-		int[] sumsOfN = new int[numbers.length];
-		sumsOfN[0] = numbers[0];
-		// 누적합계배열 생성
-		// 모배열의 변화가 없을 경우만 사용가능
-		// 변화 발생시 누적합계과정 다시 진행.
-		for(int i = 1; i < numbers.length; ++i) {
-			sumsOfN[i] = sumsOfN[i-1] + numbers[i];
-		}
+	private static void solve(int[] numbers, Command[] commands, int limit) {
+		// create segment Tree
+		int n = numbers.length;
+		double log2N = Math.log(n) / Math.log(2);  
+		int h = (int)Math.ceil(log2N);
+		long[] segTree = new long[(1 << (h+1))]; 
+		init(numbers, segTree, 1, 0, n-1);
 		
-		for(int i = 0; i < nOfQ; ++i) {
-			int command = questions[i][0];
-			int src = questions[i][1] - 1;
-			int dst = questions[i][2] - 1;
-			
-			if(command == 2) {
-				System.out.println(sumsOfN[dst] - sumsOfN[src-1]);
-			}
-			else {
-				// 모배열의 변화가 발생하게 되므로
-				// 누적합 배열생성 만으로 해결할 경우
-				// tle 발생
+		for (int i = 0; i < commands.length; ++i) {
+			Command c = commands[i];
+			switch (c.command) {
+			case 1:
+				c.src -= 1;
+				int val = c.dst - numbers[c.src];
+				numbers[c.src] = val;
+				update(segTree, 1, 0, n-1, c.src, val);
+				break;
+			case 2:
+				long sum = sum(segTree, 1, 0, n-1, c.src-1, c.dst-1);
+				System.out.println(sum);
+				break;
+			default:
+				break;
 			}
 		}
 	}
 
+	private static long init(int[] numbers, long[] segTree, int node, int start, int end) {
+		if (start == end) {
+			return segTree[node] = numbers[start];
+		}
+		else {
+			return segTree[node] = init(numbers, segTree, node*2, start, (start+end)/2) 
+									+ init(numbers, segTree, node*2+1, (start+end)/2+1, end);
+		}
+	}
+	
+	private static long sum(long[] segTree, int node, int start, int end, int left, int right) {
+		if (left > end || right < start) {
+			return 0;
+		}
+		if (left <= start && right >= end) {
+			return segTree[node];
+		}
+		return sum(segTree, node*2, start, (start+end)/2, left, right) +
+			   sum(segTree, node*2+1, (start+end)/2+1, end, left, right);
+	}
+	
+	// lazy propagation
+	private static void update(long[] segTree, int node, int start, int end, int index, long val) {
+		if (index < start || index > end) {
+			return;
+		}
+		segTree[node] = segTree[node] + val;
+		if (start != end) {
+			update(segTree, node*2, start, (start+end)/2, index, val);
+			update(segTree, node*2+1, (start+end)/2+1, end, index, val);
+		}
+	}
+}
+
+class Command {
+	int command;
+	int src;
+	int dst;
+	
+	public Command(int command, int src, int dst) {
+		this.command = command;
+		this.src = src;
+		this.dst = dst;
+	}
 }

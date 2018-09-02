6bcde032dd568d17b9d40a5f7b3fc718475c35a7
nadara9209 <nadara9209@gmail.com>
2018-09-02 17:48:39 +0900

    using indegrees Array

--- problem_solve/src/Main.java
+++ problem_solve/src/Main.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
@@ -9,19 +11,47 @@ public class Main {
 		int N = scan.nextInt();
 		int M = scan.nextInt();
 		indegrees = new int[N+1];
-		adjList = new (ArrayList<Integer>)ArrayList[N+1];
+		adjList = (ArrayList<Integer>[]) new ArrayList[N+1];
 		for (int i = 1; i < N+1; ++i) {
 			adjList[i] = new ArrayList<>();
 		}
 		
-		for(int i = 0; i < M; ++i) {
+		for (int i = 0; i < M; ++i) {
 			int studentA = scan.nextInt();
 			int studentB = scan.nextInt();
 			adjList[studentA].add(studentB);
 			indegrees[studentB]++;
 		}
 		
-		solve();
+		solve(N, M);
 		scan.close();
+	}
+	
+	private static void solve(int n, int m) {
+		ArrayList<Integer> retList = new ArrayList<>();
+		Queue<Integer> q = new LinkedList<>();
+		for (int i = 1; i <= n; ++i) {
+			if(indegrees[i] == 0) {
+				q.offer(i);
+			}
+		}
+		
+		while (!q.isEmpty()) {
+			int studentA = q.poll();
+			if (indegrees[studentA] == 0) {
+				retList.add(studentA);
+				for (int i = 0; i < adjList[studentA].size(); ++i) {
+					int studentB = adjList[studentA].get(i);
+					indegrees[studentB]--;
+					if(indegrees[studentB] == 0) {
+						q.offer(studentB);
+					}
+				}
+			}
+		}
+		
+		for (int i = 0; i < retList.size(); ++i) {
+			System.out.print(retList.get(i) + " ");
+		}
 	}
 }

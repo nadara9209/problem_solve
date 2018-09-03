8a1235eeafc7f37a7dd7115e6fe1306d540ade92
nadara9209 <nadara9209@gmail.com>
2018-09-03 16:40:27 +0900

    use Kruskal's Algorithm

--- problem_solve/src/Main.java
+++ problem_solve/src/Main.java
@@ -1,67 +1,86 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	static int[] indegrees;
-	static ArrayList<Integer>[] adjList;
+	static int[] computers;
+	static Line[] lines;
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int N = scan.nextInt();
 		int M = scan.nextInt();
-		indegrees = new int[N+1];
-		adjList = new ArrayList[N+1];
-		for (int i = 1; i < N+1; ++i) {
-			adjList[i] = new ArrayList<>();
-		}
+		computers = new int[N+1];
+		init();
 		
+		lines = new Line[M];
 		for (int i = 0; i < M; ++i) {
-			int studentA = scan.nextInt();
-			int studentB = scan.nextInt();
-			// 간선 정보 저장
-			adjList[studentA].add(studentB);
-			// 선수 정보 저장
-			indegrees[studentB]++;
-		}
+			int prev = scan.nextInt();
+			int next = scan.nextInt();
+			int cost = scan.nextInt();
+			lines[i] = new Line(prev, next, cost);
+ 		}
 		
-		solve(N, M);
+		int answer = solve();
+		System.out.println(answer);
 		scan.close();
 	}
 	
-	private static void solve(int n, int m) {
-		ArrayList<Integer> retList = new ArrayList<>();
-		Queue<Integer> q = new LinkedList<>();
-		// 초기 선수가 없는 학생을 큐에 넣고
-		for (int i = 1; i <= n; ++i) {
-			if (indegrees[i] == 0) {
-				q.offer(i);
+	private static int solve() {
+		int ret = 0;
+		Arrays.sort(lines);
+		for (int i = 0; i < lines.length; ++i) {
+			Line tmp = lines[i];
+			if (find(tmp.prevCom) != find(tmp.nextCom)) {
+				union(tmp.prevCom, tmp.nextCom);
+				ret += tmp.cost;
+			}
+			else {
+				continue;
 			}
 		}
-		
-		// bfs
-		while (!q.isEmpty()) {
-			int studentA = q.poll();
-			// 선수가 없다면
-			if (indegrees[studentA] == 0) {
-				// 리턴
-				retList.add(studentA);
-				for (int i = 0; i < adjList[studentA].size(); ++i) {
-					// 선수가 없어 리턴된 정점의 간선 관계인 정점들을 뽑아
-					int studentB = adjList[studentA].get(i);
-					// 관계 삭제
-					indegrees[studentB]--;
-					// 관계 삭제 후 선수정점의 갯수가 0이 되어버린 정점들을 다시 큐에 넣음
-					if (indegrees[studentB] == 0) {
-						q.offer(studentB);
-					}
-					// 선수가 남아 있다면 무시. 어짜피 다시 만나게 됨.
-				}
-			}
+		return ret;
+	}
+
+	private static void union(int prevCom, int nextCom) {
+		int rootOfPrev = find(prevCom);
+		int rootOfNext = find(nextCom);
+		computers[rootOfPrev] = rootOfNext;
+	}
+
+	private static int find(int prevCom) {
+		if (computers[prevCom] == prevCom) {
+			return prevCom;
 		}
-		
-		for (int i = 0; i < retList.size(); ++i) {
-			System.out.print(retList.get(i) + " ");
+		return computers[prevCom] = find(computers[prevCom]);
+	}
+
+	private static void init() {
+		for (int i = 1; i < computers.length; ++i) {
+			computers[i] = i;
+		}
+	}
+}
+
+class Line implements Comparable<Line>{
+	int prevCom;
+	int nextCom;
+	int cost;
+	
+	public Line(int p, int n, int cost) {
+		this.prevCom = p;
+		this.nextCom = n;
+		this.cost = cost;
+	}
+
+	@Override
+	public int compareTo(Line o) {
+		if (this.cost < o.cost) {
+			return -1;
+		}
+		else if (this.cost > o.cost) {
+			return 1;
+		}
+		else {
+			return 0;
 		}
 	}
 }

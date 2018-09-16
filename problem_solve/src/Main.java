a01bb8f7cbd3d06f2a6b4934a5736a2f57ec1a9f
nadara9209 <nadara9209@gmail.com>
2018-09-16 10:26:40 +0900

    demo_04

--- problem_solve/src/Main.java
+++ problem_solve/src/Main.java
@@ -1,12 +1,9 @@
 public class Main {
 	public static void main(String[] args) {
 		Solution sol = new Solution();
-		String[] record = { "Enter uid1234 Muzi", 
-				  			"Enter uid4567 Prodo", 
-				  			"Leave uid1234", 
-				  			"Enter uid1234 Prodo", 
-				  			"Change uid4567 Ryan"
-						  };
-		sol.solution(record);
+		int[] food_times = {3, 1, 2};
+		long k = 5;
+		sol.solution(food_times, k);
 	}
 }
+
--- problem_solve/src/Solution.java
+++ problem_solve/src/Solution.java
@@ -1,68 +1,42 @@
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedList;
 
 class Solution {
-	static String[] expr = {"님이 들어왔습니다.",
-					   	    "님이 나갔습니다."};
-    public String[] solution(String[] record) {
-    	HashMap<String, String> userMap = new HashMap<>();
-    	// order
-    	ArrayList<Order> orderList = new ArrayList<>();
-    	String[] answer = solve(record, userMap, orderList);
+	static LinkedList<Food> food_list;
+    public int solution(int[] food_times, long k) {
+        int answer = solve(food_times, k);
+        System.out.println(answer);
         return answer;
     }
 
-	private String[] solve(String[] record, HashMap<String, String> userMap, ArrayList<Order> orderList) {
-		String[] answer;
-		for (int i = 0; i < record.length; ++i) {
-			String str = record[i];
-			String[] temp = str.split(" ");
-			
-			String command = temp[0];
-			String uId = temp[1];
-			String name = "";
-			if (temp.length == 3) {
-				name = temp[2];
-			}
-			
-			switch (command.charAt(0)) {
-			case 'E':
-				userMap.put(uId, name);
-				orderList.add(new Order(uId, 0));
-				break;
-			case 'L':
-				orderList.add(new Order(uId, 1));
-				break;
-			case 'C':
-				userMap.put(uId, name);
-				break;
-			default:
-				break;
-			}
-		}
-		
-		answer = new String[orderList.size()];
-		
-		for (int i = 0; i < orderList.size(); ++i) {
-			String uId = orderList.get(i).uId;
-			int exprNum = orderList.get(i).exprNum;
-			if (exprNum == 0) {
-				answer[i] = userMap.get(uId) + expr[exprNum];
-			}
-			else {
-				answer[i] = userMap.get(uId) + expr[exprNum];
-			}
-		}
-		return answer;
-	}
+    private int solve(int[] food_times, long k) {
+        long cnt = 0;
+        food_list = new LinkedList<>();
+        for (int i = 0; i < food_times.length; ++i) {
+        	food_list.add(new Food(i+1, food_times[i]));
+        }
+        
+        while (cnt < k) {
+        	if(food_list.isEmpty()) {
+            	return -1;
+            }
+            Food food = food_list.removeFirst();
+            food.time -= 1;
+            if(food.time != 0) {
+            	food_list.addLast(food);
+            }
+            cnt++;
+        }
+        
+        return food_list.getFirst().num;
+    }
 }
 
-class Order {
-	String uId;
-	int exprNum;
+class Food {
+	int num;
+	int time;
 	
-	public Order(String uId, int exprNum) {
-		this.uId = uId;
-		this.exprNum = exprNum;
+	public Food(int num, int time) {
+		this.num = num;
+		this.time = time;
 	}
 }

85c6833a7b7d436a7cb99890cfc95dfc977a8484
nadara9209 <nadara9209@gmail.com>
2018-09-22 15:55:16 +0900

    solve

--- problem_solve/src/Main.java
+++ problem_solve/src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
 	public static void main(String[] args) {
 		Solution sol = new Solution();
-		final int[] healths = {200, 120, 150}; 
-		final int[][] items = {{30, 100}, {500, 30}, {100, 400}};
-		int[] answer = sol.solution(healths, items);
+		final int N = 5;
+		final int[][] directory = {{1, 2}, {1, 3}, {2, 4}, {2, 5}};
+		final int[][] query = {{1, 5}, {2, 5}, {3, 5}, {4, 5}};
+		int[] answer = sol.solution(N, directory, query);
 		System.out.println(answer);
 	}
 }
--- problem_solve/src/Solution.java
+++ problem_solve/src/Solution.java
@@ -1,24 +1,6 @@
-import java.util.ArrayList;
-
 class Solution {
-    public int[] solution(int[] healths, int[][] items) {
-        int[] answer = solve(healths, items);
+    public int[] solution(int N, int[][] directory, int[][] query) {
+        int[] answer = {};
         return answer;
     }
-
-	private int[] solve(int[] healths, int[][] items) {
-		int[] answer = new int[healths.length];
-		int used = 0;
-		answer = permutation(answer, items, healths, 0, used);
-		return null;
-	}
-
-	private int[] permutation(int[] answer, int[][] items, int[] health, int curr, int used) {
-		for (int i = 0; i < items.length; ++i) {
-			if(((1 << curr) & i) != 1) {
-				answer
-			}
-		}
-		return null;
-	}
 }

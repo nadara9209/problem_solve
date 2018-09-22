82be23964814d863bba512ab8de30eb53b3bfa16
nadara9209 <nadara9209@gmail.com>
2018-09-22 15:36:46 +0900

    dp

--- problem_solve/src/Main.java
+++ problem_solve/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
 	public static void main(String[] args) {
 		Solution sol = new Solution();
-		final int N = 3; 
-		final int[][] house = {{0, 0}, {1, 0}, {2, 0}};
-		int answer = sol.solution(N, house);
+		final int[] healths = {200, 120, 150}; 
+		final int[][] items = {{30, 100}, {500, 30}, {100, 400}};
+		int[] answer = sol.solution(healths, items);
 		System.out.println(answer);
 	}
 }
--- problem_solve/src/Solution.java
+++ problem_solve/src/Solution.java
@@ -1,65 +1,24 @@
 import java.util.ArrayList;
 
 class Solution {
-    public int solution(int N, int[][] house) {
-    	final int[][] location = new int[201][201];
-    	final ArrayList<Point> pList = new ArrayList<>();
-    	int answer = solve(0, 0, 0, location, house, N, pList);
+    public int[] solution(int[] healths, int[][] items) {
+        int[] answer = solve(healths, items);
         return answer;
     }
 
-	private int solve(int i, int j, int n, int[][] location, int[][] house, int limit, ArrayList<Point> pList) {
-		if (n == limit) {
-			return computeMin(pList, house);
-		}
-		
-		int ret = 0;
-		int row = i;
-		int col = j;
-		for (; row < location.length; ++row) {
-			for (; col < location[0].length; ++col) {
-				if(!(row == 0 || row == location.length-1 || col == 0 || col == location.length-1)) {
-					continue;
-				}
-				if(location[row][col] != 1) {
-					location[row][col] = 1;
-					pList.add(new Point(row, col));
-					int tmp = solve(row, col, n+1, location, house, limit, pList); 
-					if (ret < tmp) {
-						ret = tmp;
-					}
-					location[row][col] = 0;
-					pList.remove(pList.size()-1);
-				}		
+	private int[] solve(int[] healths, int[][] items) {
+		int[] answer = new int[healths.length];
+		int used = 0;
+		answer = permutation(answer, items, healths, 0, used);
+		return null;
+	}
+
+	private int[] permutation(int[] answer, int[][] items, int[] health, int curr, int used) {
+		for (int i = 0; i < items.length; ++i) {
+			if(((1 << curr) & i) != 1) {
+				answer
 			}
 		}
-		return ret;
-	}
-
-	private int computeMin(ArrayList<Point> pList, int[][] house) {
-		int ret = Integer.MAX_VALUE;
-		for (Point p : pList) {
-			for (int row_h = 0; row_h < house.length; ++row_h) {
-				int tmp_h = compute(p.x-100, p.y-100, house[row_h][0], house[row_h][1]);
-				if (ret > tmp_h) {
-					ret = tmp_h;
-				}
-			}
-		}
-		return ret;
-	}
-
-	private int compute(int i, int j, int k, int l) {
-		return (int)(Math.pow(Math.abs(i-k), 2) + Math.pow(Math.abs(j-k), 2));
-	}
-}
-
-class Point {
-	int x;
-	int y;
-	
-	public Point(int x, int y) {
-		this.x = x;
-		this.y = y;
+		return null;
 	}
 }

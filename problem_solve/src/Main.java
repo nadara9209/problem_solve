9d97b3aa8acdceff22c9e0f2712849ba2d17c7cf
nadara9209 <nadara9209@gmail.com>
2018-09-22 11:38:20 +0900

    Modify lg_cns_test_1

--- problem_solve/src/Main.java
+++ problem_solve/src/Main.java
@@ -1,15 +1,8 @@
 public class Main {
 	public static void main(String[] args) {
 		Solution sol = new Solution();
-		
-		final String[][] INPUT_relation = {{"100", "ryan", "music", "2"}, 
-										   {"200", "apeach", "math", "2"}, 
-										   {"300", "tube", "computer", "3"}, 
-										   {"400", "con", "computer", "4"}, 
-										   {"500", "muzi", "music", "3"}, 
-										   {"600", "apeach", "music", "2"}};
-		
-		int answer = sol.solution(INPUT_relation);
+		final int[] INPUT = {0, 1, 2}; 
+		boolean answer = sol.solution(INPUT);
 		System.out.println(answer);
 	}
 }
--- problem_solve/src/Solution.java
+++ problem_solve/src/Solution.java
@@ -1,101 +1,34 @@
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Set;
 
 class Solution {
-    public int solution(String[][] relation) {
-        int answer = solve(relation);
+    public boolean solution(int[] arr) {
+        boolean answer = true;
+        answer = solve(arr);
+        process();
         return answer;
     }
 
-	private int solve(String[][] relation) {
-		List<Set<Integer>> categoryList;
-		int limit = relation[0].length;
-		categoryList = createList_1(limit);
+	private void process() {
+		int n = 10;
+		ArrayList<Integer>[] list = new ArrayList<>()[];
+		int[] arr = new int[n];
 		
-		int nOfCandidateKey = count(categoryList, relation);
-		return nOfCandidateKey;
 	}
 
-	private int count(List<Set<Integer>> dbSet, String[][] relation) {
-		int retCnt = 0;
-		for (int i = 0; i < dbSet.size(); ++i) {
-			Set<Integer> categoryA = dbSet.get(i);
-			Set<String> strSet = new HashSet<>();
-			boolean isUnique = true;
-			for (int row = 0; row < relation.length; ++row) {
-				String str = "";
-				for (int j : categoryA) {
-					str += relation[row][j] + "_";
-				}
-				if (strSet.contains(str)) {
-					isUnique = false;
-					break;
-				}
-				strSet.add(str);
+	private boolean solve(int[] arr) {
+		Set<Integer> set = new HashSet<>();
+		int limit = arr.length;
+		for (int number : arr) {
+			if (number == 0 || number > limit) {
+				return false;
 			}
-			
-			if(isUnique) {
-				retCnt++;
-				dbSet.removeIf((Set<Integer> categoryB) -> categoryB.containsAll(categoryA));
+			if (set.contains(number)) {
+				return false;
 			}
+			set.add(number);
 		}
-		return retCnt;
+		return true;
 	}
-	
-	private List<Set<Integer>> createList(int n) {
-		List<Set<Integer>> retList = new LinkedList<>();
-		for (int i = 1; i < (1 << n); ++i) {
-			Set<Integer> tmpSet = new HashSet<>();
-			
-			int attributeId = 0;
-			for (int j = i; j != 0; j >>>= 1) {
-				if ((j & 1) != 0) {
-					tmpSet.add(attributeId);
-				}
-				attributeId += 1;
-			}
-			retList.add(tmpSet);
-		}
-		return retList;
-	}
-	
-	private List<Set<Integer>> createList_1(int n) {
-		List<Set<Integer>> retList = new LinkedList<>();
-		for (int i = 1; i < (1 << n); ++i) {
-			Set<Integer> tmpSet = new HashSet<>();
-			
-			for (int j = 0; j < n; ++j) {
-				if ((i & (1 << j)) != 0) {
-					tmpSet.add(j);
-				}
-			}
-			retList.add(tmpSet);
-		}
-		return retList;
-	}
-	
-//	private Set<Integer> createDB_1(int n) {
-//		Set<Integer> retSet = new HashSet<>();
-//		for (int i = 1; i < (1 << n); ++i) {
-//			int attributeId = 0;
-//			int sumOfAttributeId = 0;
-//			for (int j = i; j != 0; j >>>= 1) {
-//				if ((j & 1) != 0) {
-//					sumOfAttributeId += attributeId;
-//				}
-//				attributeId <<= 1;
-//			}
-//			retSet.add(sumOfAttributeId);
-//		}
-//		
-//		Iterator<Integer> iter = retSet.iterator();
-//		while(iter.hasNext()) {
-//			Integer i = iter.next();
-//			System.out.println(i);
-//		}
-//		
-//		return retSet;
-//	}
 }

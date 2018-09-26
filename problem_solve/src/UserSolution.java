88ca2dc871079feaf0fcd41417a6d6794f7ad098
nadara9209 <nadara9209@gmail.com>
2018-09-26 22:50:59 +0900

    s

--- problem_solve/src/UserSolution.java
+++ problem_solve/src/UserSolution.java
@@ -1,20 +1,41 @@
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
 class UserSolution {
     public final static int N = 4;
     public static int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    public static Set<Integer[]> candidateSet;
+    public static Set<int[]> candidatesSet;
 
     public void doUserImplementation(int guess[]) {
-        candidateSet = createAllSet();
-        
+        createAllSet();
+        int n = candidatesSet.size();
+        int a = 0;
+        //guess = solve();
     }
 
-	private Set<Integer[]> createAllSet() {
-		// Use permutation to create powerSet
-		// Set iterator remove
-		Set<Integer[]> retSet = new HashSet<>();
-		return null;
+	private void createAllSet() {
+		candidatesSet = new HashSet<>();
+		ArrayList<Integer> numList = new ArrayList<>();
+		for (int number : numbers) {
+			numList.add(number);
+		}
+		int[] candidate = new int[N];
+		createAllSet(candidate, numList, 0);
 	}
+
+	private void createAllSet(int[] candidate, ArrayList<Integer> numList, int id) {
+		if (id == N) {
+			candidatesSet.add(candidate);
+			return;
+		}
+		
+		for (int i = 0; i < numbers.length - id; ++i) {
+			candidate[id] = numList.remove(i);
+			createAllSet(candidate, numList, id+1);
+			numList.add(i, candidate[id]);
+		}
+	}
+
+	
 }

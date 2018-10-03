d1152433e0d0d76500e8f64ebfbcda38274d54ac
nadara9209 <nadara9209@gmail.com>
2018-10-03 10:23:42 +0900

    x, y좌표 값을 입력받을때 x축 대칭인 것을 생각하자.

--- problem_solve/src/Solution.java
+++ problem_solve/src/Solution.java
@@ -5,43 +5,30 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Solution {
-	static int[] dRow = {-1,  0,  1,  0};
-	static int[] dCol = { 0, -1,  0,  1};
+	static List<Atom> atomList;
+	static final int offset = 2000;
 	
-	static final int UP = 0;
-	static final int LEFT = 1;
-	static final int DOWN = 2;
-	static final int RIGHT = 3;
-	static int[] opposite = {DOWN, RIGHT, UP, LEFT};
-	static int[][] nextDir;
-	
-	static Map<Integer, List<Point>> wormholes;
-	
-	static int[][] map;
-	static int N;
+	// 실제 x, y에 대응하기 위해 상/하 방향 반대 
+	static int[] dRow = { 1, -1,  0,  0 };
+	static int[] dCol = { 0,  0, -1,  1 };
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int T = scan.nextInt();
 		for (int tc = 1; tc <= T; ++tc) {
-			N = scan.nextInt();
-			map = new int[N][N];
+			int N = scan.nextInt();
+			atomList = new ArrayList<>();
 			
-			wormholes = new HashMap<>();
-			
-			for (int row = 0; row < N; ++row) {
-				for (int col = 0; col < N; ++col) {
-					int val = scan.nextInt();
-					if (val >= 6 && val <= 10) {
-						if (wormholes.containsKey(val)) {
-							wormholes.get(val).add(new Point(row, col));
-						} else {
-							wormholes.put(val, new ArrayList<>());
-							wormholes.get(val).add(new Point(row, col));
-						}
-					}
-					map[row][col] = val;
-				}
+			for (int i = 0; i < N; ++i) {
+				int col = scan.nextInt();
+				int row = scan.nextInt();
+				int dir = scan.nextInt();
+				int energy = scan.nextInt();
+				
+				// 0.5초 충돌 체크를 위한 좌표 확장
+				int changedRow = (row * 2) + offset;
+				int chaneedCol = (col * 2) + offset;
+				atomList.add(new Atom(new Point(changedRow, chaneedCol), dir, energy));
 			}
 			
 			int answer = solve();
@@ -51,119 +38,52 @@ public class Solution {
 	}
 
 	private static int solve() {
-		InitNextDir();
-		int answer = countAllCases();
-		return answer;
+		int maxEnergy = checkAllCases();
+		return maxEnergy;
 	}
 
-
-	private static void InitNextDir() {
-		nextDir = new int[6][4];
-		nextDir[1][UP] = opposite[UP];
-		nextDir[1][LEFT] = UP;
-		nextDir[1][DOWN] = RIGHT;
-		nextDir[1][RIGHT] = opposite[RIGHT];
+	private static int checkAllCases() {
+		int totalEnergy = 0;
 		
-		nextDir[2][UP] = RIGHT;
-		nextDir[2][LEFT] = DOWN;
-		nextDir[2][DOWN] = opposite[DOWN];
-		nextDir[2][RIGHT] = opposite[RIGHT];
-		
-		nextDir[3][UP] = LEFT;
-		nextDir[3][LEFT] = opposite[LEFT];
-		nextDir[3][DOWN] = opposite[DOWN];
-		nextDir[3][RIGHT] = DOWN;
-		
-		nextDir[4][UP] = opposite[UP];
-		nextDir[4][LEFT] = opposite[LEFT];
-		nextDir[4][DOWN] = LEFT;
-		nextDir[4][RIGHT] = UP;
-		
-		nextDir[5][UP] = opposite[UP];
-		nextDir[5][LEFT] = opposite[LEFT];
-		nextDir[5][DOWN] = opposite[DOWN];
-		nextDir[5][RIGHT] = opposite[RIGHT];
-	}
-
-	private static int countAllCases() {
-		int ret = 0;
-		for (int row = 0; row < N; ++row) {
-			for (int col = 0; col < N; ++col) {
-				int val = map[row][col];
-				if (val == 0) {
-					int tmp = countScore(row, col);
-					if (ret < tmp) {
-						ret = tmp;
+		while (!atomList.isEmpty()) {
+			// 충돌 정보 저장 map
+			Map<Point, List<Atom>> collisionMap = new HashMap<>();
+			
+			// 이동
+			for (Atom a : atomList) {
+				a.move();
+				Point currP = a.p;
+				
+				if (!collisionMap.containsKey(currP)) {
+					List<Atom> candidateList = new ArrayList<>();
+					candidateList.add(a);
+					collisionMap.put(currP, candidateList);
+				}
+				else {
+					collisionMap.get(currP).add(a);
+				}
+			}
+			
+			// 충돌 및 소멸, 충돌 불가능 처리
+			for (Point p : collisionMap.keySet()) {
+				List<Atom> candidateList = collisionMap.get(p);
+				if (!isValid(p)) { // 충돌 불가능
+					for (Atom a : candidateList) {
+						atomList.remove(a);
+					}
+				}
+				else { // 충돌 및 소멸
+					if (candidateList.size() > 1) {
+						for (Atom a : candidateList) {
+							atomList.remove(a);
+							totalEnergy += a.energy;
+						}
 					}
 				}
 			}
 		}
-		return ret;
-	}
-
-	private static int countScore(int row, int col) {
-		Point firstP = new Point(row, col);
-		int ret = 0;
-		for (int i = 0; i < 4; ++i) {
-			int tmp = count(firstP, i);
-			if (ret < tmp) {
-				ret = tmp;
-			}
-		}
 		
-//		int ret = count(new Point(7, 0), UP);
-		return ret;
-	}
-
-	private static int count(Point firstP, int Dir) {
-		int score = 0;
-
-		Point currP = new Point(firstP);
-		do {
-//			Point prevP = new Point(currP);
-			currP.row += dRow[Dir];
-			currP.col += dCol[Dir];
-			
-			if (!isValid(currP)){
-				// 벽
-				// 방향 반대 원래 자리 스코어 증가
-				Dir = opposite[Dir];
-				score++;
-				continue;
-			}
-			
-			int flag = map[currP.row][currP.col];
-			
-			if (flag == -1) {
-				return score;
-			}
-			else if (flag >= 1 && flag <= 5) {
-				// 블럭
-				// 방향 변화, 좌표 고정
-				Dir = nextDir[flag][Dir];
-				score++;
-			}
-			else if (flag >= 6 && flag <= 10) {
-				// 웜홀
-				// 맵 상의 같은 웜홀 번호의 좌표로 변화, 방향 고정
-				Point wormholeA = wormholes.get(flag).get(0);
-//				System.out.println(wormholeA.row);
-//				System.out.println(wormholeA.col);
-				Point wormholeB = wormholes.get(flag).get(1);
-//				System.out.println(wormholeB.row);
-//				System.out.println(wormholeB.col);
-
-				Point targetP = (currP.equals(wormholeA)) ? wormholeB : wormholeA;
-				currP.set(targetP);
-			}
-			else {
-				// 빈공간 혹은 시작점
-			}
-			
-			// 시작점과, 블랙홀을 반복문의 시작에서 검사
-		}while (!currP.equals(firstP));
-		
-		return score;
+		return totalEnergy;
 	}
 
 	private static boolean isValid(Point p) {
@@ -171,35 +91,57 @@ public class Solution {
 	}
 
 	private static boolean isValid(int row, int col) {
-		return (row >= 0 && col >= 0 && row < N && col < N);
-	}
-}
-
-class Point {
-	int row;
-	int col;
-
-	public Point(int row, int col) {
-		this.row = row;
-		this.col = col;
-	}
-	
-	public Point(Point p) {
-		this(p.row, p.col);
-	}
-	
-	public Point() {
-		this(0, 0);
-	}
-	
-	public void set(Point p) {
-		this.row = p.row;
-		this.col = p.col;
+		return (row >= 0 && row <= 4001 && col >= 0 && col <= 4001);
 	}
 
-	public boolean equals(Point p) {
-		if (this.row != p.row) return false;
-		if (this.col != p.col) return false;
-		return true;
+	static class Atom {
+		Point p;
+		int dir;
+		int energy;
+		
+		public Atom(Point p, int dir, int energy) {
+			this.p = p;
+			this.dir = dir;
+			this.energy = energy;
+		}
+	
+		public void move() {
+			this.p.move(this.dir);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (obj instanceof Atom) {
+				return this.p.equals(((Atom) obj).p);
+			}
+			return super.equals(obj);
+		}
+	}
+	
+	static class Point {
+		int row;
+		int col;
+		
+		public Point(int row, int col) {
+			this.row = row;
+			this.col = col;
+		}
+		
+		public void move(int dir) {
+			this.row += dRow[dir];
+			this.col += dCol[dir];
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this.row != ((Point) obj).row) return false;
+			if (this.col != ((Point) obj).col) return false;
+			return true;
+		}
+		
+		@Override
+		public int hashCode() {
+			return (this.row * 111111) + this.col;
+		}
 	}
 }

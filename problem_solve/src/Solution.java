75961ec3c8bd5713c55c882eeb1e5d44ec0b6270
nadara9209 <nadara9209@gmail.com>
2018-10-07 20:40:09 +0900

    whowho

--- problem_solve/src/Solution.java
+++ problem_solve/src/Solution.java
@@ -1,120 +1,155 @@
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
-	static Wheel[] wheels = new Wheel[5];
-	
+	static int D;
+	static int W;
+	static int K;
+	static int[][] film;
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int T = scan.nextInt();
 		for (int tc = 1; tc <= T; ++tc) {
-			int cnt = scan.nextInt();
+			D = scan.nextInt();
+			W = scan.nextInt();
+			K = scan.nextInt();
 			
-			for (int i = 1; i <= 4; i++) {
-				wheels[i] = new Wheel();
-				for (int j = 0; j < 8; ++j) {
-					wheels[i].gears.addLast(scan.nextInt());
+			film = new int[D][W];
+			for (int row = 0; row < D; ++row) {
+				for (int col = 0; col < W; ++col) {
+					film[row][col] = scan.nextInt();
 				}
 			}
 			
-			for (int i = 1; i <= 3; i++) {
-				wheels[i].setRightWheel(wheels[i+1]);
-				wheels[i+1].setLeftWheel(wheels[i]);
-			}
-			
-			for (int i = 0; i < cnt; i++) {
-				int wheelId = scan.nextInt();
-				int direction = scan.nextInt();
-				
-				wheels[wheelId].rotate(direction);
-			}
-			
-			int answer = calculateScore();
+			int answer = solve();
 			System.out.println("#" + tc + " " + answer);
 		}
-		
 		scan.close();
 	}
-
-	private static int calculateScore() {
-		int totalScore = 0;
-
-		totalScore += wheels[1].getTop();
-		totalScore += wheels[2].getTop() * 2;
-		totalScore += wheels[3].getTop() * 4;
-		totalScore += wheels[4].getTop() * 8;
-		
-		return totalScore;
-	}
-}
-
-class Wheel {
-	public LinkedList<Integer> gears;
-	private Wheel leftWheel;
-	private Wheel rightWheel;
-
-	public Wheel() {
-		this.gears = new LinkedList<>();
-		this.leftWheel = null;
-		this.rightWheel = null;
-	}
-
-	public void setLeftWheel(Wheel wheel) {
-		this.leftWheel = wheel;
-	}
-
-	public void setRightWheel(Wheel wheel) {
-		this.rightWheel = wheel;
-	}
-
-	public void rotate(int direction) {
-		this.rotate(this, direction);
-	}
-
-	public void rotate(Wheel sourceWheel, int direction) {
-		if (this.hasLeftWheel() && this.leftWheel != sourceWheel) {
-			if (this.getLeft() != this.leftWheel.getRight()) {
-				this.leftWheel.rotate(this, -direction);
-			}
-		}
-		if (this.hasRightWheel() && this.rightWheel != sourceWheel) {
-			if (this.getRight() != this.rightWheel.getLeft()) {
-				this.rightWheel.rotate(this, -direction);
-			}
-		}
-		
-		if (direction > 0)
-			this.turnRight();
-		else
-			this.turnLeft();
-	}
-
-	private void turnLeft() {
-		this.gears.addLast(this.gears.removeFirst());
-	}
-
-	private void turnRight() {
-		this.gears.addFirst(this.gears.removeLast());
-	}
-
-	private boolean hasLeftWheel() {
-		return (this.leftWheel != null);
-	}
-
-	private boolean hasRightWheel() {
-		return (this.rightWheel != null);
-	}
-
-	public int getRight() {
-		return this.gears.get(2);
-	}
-
-	public int getLeft() {
-		return this.gears.get(6);
-	}
-
-	public int getTop() {
-		return this.gears.get(0);
-	}
 	
+	private static int solve() {
+		int minNOfInjections = countAllCases();
+		return minNOfInjections;
+	}
+
+	private static int countAllCases() {
+		List<Integer> idList = new ArrayList<>();
+		for (int i = 0; i < D; ++i) {
+			idList.add(i);
+		}
+		
+		List<Integer> candidateList = new ArrayList<>();
+		
+		int nOfInjections;
+		for (nOfInjections = 0; nOfInjections < D; ++nOfInjections) {
+			if (isPassed(candidateList, idList, 0, nOfInjections)) {
+				break;
+			}
+		}
+		return nOfInjections;
+	}
+
+	private static boolean isPassed(List<Integer> candidateList, List<Integer> idList, int i, int nOfInjections) {
+		if (candidateList.size() == nOfInjections) {
+			return check(candidateList);
+		}
+		
+		if (i >= idList.size()) {
+			return false;
+		}
+		
+		boolean caseA;
+		boolean caseB;
+		
+		candidateList.add(idList.get(i));
+		caseA = isPassed(candidateList, idList, i+1, nOfInjections);
+		if (caseA) {
+			return true;
+		}
+		candidateList.remove(candidateList.size()-1);
+		
+		caseB = isPassed(candidateList, idList, i+1, nOfInjections);
+		if (caseB) {
+			return true;
+		}
+		
+		return false;
+	}
+
+	private static boolean check(List<Integer> candidateList) {
+		int limit = candidateList.size();
+		for (int i = 0; i < (1 << limit); ++i) {
+			boolean[] flag = new boolean[limit];
+			for (int j = i; j >= 0; j >>>= 1) {
+				if ((j & 1) != 0) {
+					flag[i] = true;
+				}
+			}
+			
+			// injections
+			int[][] injectedFilm = injection(flag, candidateList);
+			// check ispassed
+			if (checkPassed(injectedFilm)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+	private static boolean checkPassed(int[][] injectedFilm) {
+		for (int col = 0; col < W; ++col) {
+			int cntA = 0;
+			int cntB = 0;
+			boolean isPassedA = false;
+			boolean isPassedB = false;
+			for (int row = 0; row < D; ++row) {
+				if (injectedFilm[row][col] == 0) {
+					cntA++;
+					if (cntA == K) {
+						isPassedA = true;
+					}
+				}
+				else {
+					cntB++;
+					if (cntB == K) {
+						isPassedB = true;
+					}
+				}
+				
+				if (!(isPassedA || isPassedB)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	private static int[][] injection(boolean[] flag, List<Integer> candidateList) {
+		int[][] copyFilm = new int[D][W];
+		for (int row = 0; row < D; ++row) {
+			copyFilm[row] = film[row].clone();
+		}
+		
+		for (int i = 0; i < candidateList.size(); ++i) {
+			int id = candidateList.get(i);
+			fill(flag[i], id, copyFilm);
+		}
+		
+		return copyFilm;
+	}
+
+	private static void fill(boolean flag, int id, int[][] copyFilm) {
+		if (flag) {
+			for (int col = 0; col < W; ++col) {
+				copyFilm[id][col] = 0;
+			}
+		}
+		else {
+			for (int col = 0; col < W; ++col) {
+				copyFilm[id][col] = 1;
+			}
+		}
+	}
 }

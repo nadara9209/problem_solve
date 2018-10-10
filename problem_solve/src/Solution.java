adb12ae2ed1dab6c2f35a015877990d7b70b5d89
nadara9209 <nadara9209@gmail.com>
2018-10-10 18:07:21 +0900

    완탐

--- problem_solve/src/Solution.java
+++ problem_solve/src/Solution.java
@@ -1,61 +1,145 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
-	static int[] Drow = {-1,  1,  0,  0};
-	static int[] Dcol = { 0,  0, -1,  1};
+	static int N;
+	static int K;
+	static int[][] map;
+	static int highestHeight;
+	
+	static int[] dRow = { -1,  1,  0,  0 };
+	static int[] dCol = {  0,  0, -1,  1 };
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int T = scan.nextInt();
-		for(int tc = 1; tc <= T; ++tc) {
-			int N = scan.nextInt();
-			int K = scan.nextInt();
+		for (int tc = 1; tc <= T; ++tc) {
+			N = scan.nextInt();
+			K = scan.nextInt();
+			map = new int[N][N];
 			
-			int maxHeight = 0;
-			// create map
-			int[][] map = new int[N][N];
-			for(int row = 0; row < N; ++row) {
-				for(int col = 0; col < N; ++col) {
-					int heightVal = scan.nextInt();
-					map[row][col] = heightVal;
-					if(maxHeight < heightVal) {
-						maxHeight = heightVal;
+			highestHeight = 0;
+			for (int row = 0; row < N; ++row) {
+				for (int col = 0; col < N; ++col) {
+					int height = scan.nextInt();
+					map[row][col] = height;
+					if (highestHeight < height) {
+						highestHeight = height;
 					}
 				}
 			}
 			
-			int answer = solve(N, K, map, maxHeight);
+			int answer = solve();
 			System.out.println("#" + tc + " " + answer);
+			
 		}
 		scan.close();
 	}
-
-	private static int solve(int n, int k, int[][] map, int maxHeight) {
-		int maxNumberOfRoute;
-		maxNumberOfRoute = makeAllCases(n, k, map, maxHeight);
-		return maxNumberOfRoute;
+	
+	private static int solve() {
+		List<Point> highestPointList = checkHighestPoint();
+		int longestPathCount = checkPaths(highestPointList);
+		return longestPathCount;
 	}
-
-	private static int makeAllCases(int n, int k, int[][] map, int maxHeight) {
-		int ret = Integer.MAX_VALUE;
-		for(int row = 0; row < n; ++row) {
-			for(int col = 0; col < n; ++col) {
-				int heightVal = map[row][col];
-				if(heightVal == maxHeight) {
-					int tmp = countAllCases(row, col, 0, map);
-					if(ret > tmp) {
-						ret = tmp;
-					}
+	
+	private static int checkPaths(List<Point> highestPointList) {
+		int ret = 0;
+		
+		for (Point p : highestPointList) {
+			for (int i = 1; i <= K; ++i) {
+				int tmp = checkAllcases(p, i);
+				if (ret < tmp) {
+					ret = tmp;
 				}
 			}
 		}
 		return ret;
 	}
 
-	private static int countAllCases(int row, int col, int currVal, int[][] map) {
-		int nextVal = map[row][col];
-		if(currVal <= nextVal) {
-			return 1;
+	private static int checkAllcases(Point p, int diff) {
+		int ret = 0;
+		// 시작 p를 제외한 모든 map을 최대 공사가능 높이까지 깍아가면서 
+		for (int row = 0; row < N; ++row) {
+			for (int col = 0; col < N; ++col) {
+				if (!isStart(row, col, p)) {
+					map[row][col] -= diff;
+					int tmp = check(p);
+					if (ret < tmp) {
+						ret = tmp;
+					}
+					map[row][col] += diff;
+				}
+			}
 		}
-		return 0;
+		return ret;
+	}
+
+	private static int check(Point p) {
+		int ret = 0;
+		for (int dir = 0; dir < 4; ++dir) {
+			int nextRow = p.row + dRow[dir];
+			int nextCol = p.col + dCol[dir];
+			int tmp = countAllCases(p.row, p.col, nextRow, nextCol, 1);
+			if (ret < tmp) {
+				ret = tmp;
+			}
+		}
+		return ret;
+	}
+
+	private static int countAllCases(int row, int col, int nextRow, int nextCol, int pathCnt) {
+		// 범위를 벗어난 경우
+		if (!isValid(nextRow, nextCol)) {
+			return pathCnt;
+		}
+		
+		// 다음 지점이 더 높은경우
+		if (map[row][col] <= map[nextRow][nextCol]) {
+			return pathCnt;
+		}
+		
+		int ret = 0;
+		int currRow = nextRow;
+		int currCol = nextCol;
+		for (int dir = 0; dir < 4; ++dir) {
+			int tmp = countAllCases(currRow, currCol, currRow + dRow[dir], currCol + dCol[dir], pathCnt+1);
+			if (ret < tmp) {
+				ret = tmp;
+			}
+		}
+		
+		return ret;
+	}
+
+	private static boolean isValid(int row, int col) {
+		return (row >= 0 && col >= 0 && row < N && col < N);
+	}
+
+	private static boolean isStart(int row, int col, Point p) {
+		return (row == p.row && col == p.col);
+	}
+
+	private static List<Point> checkHighestPoint() {
+		List<Point> highestPointList = new ArrayList<>();
+		
+		for (int row = 0; row < N; ++row) {
+			for (int col = 0; col < N; ++col) {
+				int height = map[row][col];
+				if (height == highestHeight) {
+					highestPointList.add(new Point(row, col));
+				}
+			}
+		}
+		return highestPointList;
+	}
+
+	static class Point {
+		int row;
+		int col;
+		
+		public Point(int row, int col) {
+			this.row = row;
+			this.col = col;
+		}
 	}
 }

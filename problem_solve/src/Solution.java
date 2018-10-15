528b48d8b339f19fb25b8d3d0b9f3f7296e10839
nadara9209 <nadara9209@gmail.com>
2018-10-15 17:15:57 +0900

    주석 추가 및 수정

--- problem_solve/src/Solution.java
+++ problem_solve/src/Solution.java
@@ -48,6 +48,8 @@ public class Solution {
 
 	private static int checkAllCases(LinkedList<Person> personList, ArrayList<Stair> stairList,
 			LinkedList<Person> selectedList, int currId) {
+		// 특정 시점을 찾는 것이 아니기 때문에 조합적인 경우의 수를 모두 찾기 위해
+		// id가 더 이상 도달할 수 없을 때가 BaseCondition
 		if(currId == personList.size()) {
 			return countTime(personList, stairList, selectedList);
 		}
@@ -67,8 +69,11 @@ public class Solution {
 	private static int countTime(LinkedList<Person> personList, ArrayList<Stair> stairList,
 			LinkedList<Person> selectedList) {
 		
-		LinkedList<Person> personTempList1 = (LinkedList<Person>) personList.clone();
-		LinkedList<Person> personTempList2 = (LinkedList<Person>) selectedList.clone();
+		// 특정 ListA를 먼저 도출하고
+		// 전체 List에서 ListA를 제거하는 과정을 거쳐 ListB를 생성
+		
+		LinkedList<Person> personTempList1 = new LinkedList<>(personList);
+		LinkedList<Person> personTempList2 = new LinkedList<>(selectedList);
 		personTempList1.removeAll(personTempList2);
 		
 		Stair stair1 = stairList.get(0);
@@ -103,17 +108,20 @@ public class Solution {
 			this.reachTime = 0;
 		}
 		
+		// Person to Stair의 도달 시간을 설정
+		// 도달 시간으로 정렬한 후 빠르게 도착한 순으로 처리할 것.
 		public void setReachTime(int row, int col) {
 			this.reachTime = Math.abs(this.row - row) + Math.abs(this.col - col);
 		}
 		
+		// 오름차순 정렬
 		@Override
 		public int compareTo(Person o) {
 			if(this.reachTime < o.reachTime) {
-				return -1;
+				return 1;
 			}
 			else if(this.reachTime > o.reachTime) {
-				return 1;
+				return -1;
 			}
 			else {
 				return 0;
@@ -135,6 +143,8 @@ public class Solution {
 			this.processTime = processTime;
 			this.totalTime = 0;
 			this.candidateList = new LinkedList<>();
+			
+			// 계단 위에는 사람은 총 3명이 올라가 있을 수 있다.
 			this.stateList = new ArrayList<State>(3);
 			for(int i = 0; i < 3; ++i) {
 				stateList.add(new State());
@@ -159,12 +169,16 @@ public class Solution {
 
 		private void setTime() {
 			int time = 0;
+			
+			// 시간을 증가시키면서
 			while(true) {
 				time++;
 				for(int i = 0; i < this.stateList.size(); ++i) {
+					// 사용 못하는 애들 중에
 					if(!stateList.get(i).usable) {
-						// 기다리지 않은
+						// 기다리지 않았던 사람이라면 
 						if(!stateList.get(i).isWait) {
+							// 계단길이 + 1까지 도달할 때까지 시간이 주어진 뒤 나오게 함.
 							if((time - stateList.get(i).startTime) == this.processTime+1) {
 								stateList.get(i).usable = true;
 								if(this.candidateList.isEmpty() && this.isAllUsable()) {
@@ -173,7 +187,7 @@ public class Solution {
 								}
 							}
 						}
-						// 기다린
+						// 기다린 사람이었다면 바로 현재 계단의 길이와 비교하여 진행
 						else if(stateList.get(i).isWait) {
 							if((time - stateList.get(i).startTime) == this.processTime) {
 								stateList.get(i).usable = true;
@@ -186,8 +200,10 @@ public class Solution {
 					}
 				}
 				
-				int removeN = 0;
-				for(int i = 0; i < this.candidateList.size(); ++i) {
+				
+				// 내림차순으로 정렬해서 뒤에서부터 지워도 됨.
+				// 굳이 지울 갯수를 
+				for(int i = candidateList.size() - 1; i >= 0; --i) {
 					Person p = this.candidateList.get(i);
 					for(int j = 0; j < this.stateList.size(); ++j) {
 						if(stateList.get(j).usable) {
@@ -196,7 +212,7 @@ public class Solution {
 								stateList.get(j).startTime = time;
 								stateList.get(j).usable = false;
 								stateList.get(j).isWait = false;
-								removeN++;
+								candidateList.remove(i);
 								break;
 							}
 							// 기다린
@@ -204,15 +220,11 @@ public class Solution {
 								stateList.get(j).startTime = time;
 								stateList.get(j).usable = false;
 								stateList.get(j).isWait = true;
-								removeN++;
+								candidateList.remove(i);
 								break;
 							}
 						}
 					}
-				}
-				
-				for(int i = 0; i < removeN; ++i) {
-					candidateList.remove(0);
 				}
 			}
 		}
@@ -229,15 +241,14 @@ public class Solution {
 	
 	static class State {
 		boolean usable;
-		int startTime;
-		int consumedTime;
 		boolean isWait;
+		int startTime;
+
 		
 		State() {
 			this.usable = true;
-			this.startTime = 0;
-			this.consumedTime = 0;
 			this.isWait = true;
+			this.startTime = 0;
 		}
 	}
 }

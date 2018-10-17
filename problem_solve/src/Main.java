a1f5b1b29d4416305855a6f2a02cbef33d040bef
nadara9209 <nadara9209@gmail.com>
2018-10-17 13:33:00 +0900

    자료형과 범위

--- problem_solve/src/Main.java
+++ problem_solve/src/Main.java
@@ -1,8 +1,44 @@
 import java.util.Scanner;
 
 public class Main {
+	static int N;
+	static int B;
+	static int C;
+	static int[] testRoom;
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		N = scan.nextInt();
+		
+		testRoom = new int[N];
+		for (int i = 0; i < N; ++i) {
+			testRoom[i] = scan.nextInt();
+		}
+		
+		B = scan.nextInt();
+		C = scan.nextInt();
+		
+		long answer = solve();
+		System.out.println(answer);
 		scan.close();
+	}
+
+	private static long solve() {
+		long cnt = N;
+		for (int i = 0; i < N; ++i) {
+			int nOfPerson = testRoom[i];
+			nOfPerson -= B;
+			if (nOfPerson > 0) {
+				int ¸ò = nOfPerson / C;
+				int ³ª¸ÓÁö = nOfPerson % C;
+				if (³ª¸ÓÁö == 0) {
+					cnt += ¸ò;
+				}
+				else {
+					cnt += ¸ò + 1;
+				}
+			}
+		}
+		
+		return cnt;
 	}
 }
